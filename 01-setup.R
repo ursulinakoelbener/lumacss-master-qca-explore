28ff44f6411339fc56b008094d016325df81c160
Ursulina Kölbener <ursulina@koelbener.ch>
2024-03-12 17:22:47 +0100

    add command for GUI

--- 01-setup.R
+++ 01-setup.R
@@ -1,5 +1,9 @@
 # Install the QCA package from the Comprehensive R Archive Network (CRAN)
 install.packages("QCA")
+install.packages("admisc")
 
 # Load the QCA package into the R session
 library(QCA)
+
+# start graphical user interface
+runGUI()
